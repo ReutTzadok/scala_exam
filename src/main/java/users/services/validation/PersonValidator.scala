5ee4171c71868663a155cd279c34d0d83d7b79a2
Reut Tzadok <reut.b1212@gmail.com>
2021-05-30 21:27:50 +0300

    final

--- src/main/java/users/services/validation/PersonValidator.scala
+++
@@ -1,8 +0,0 @@
-package users.services.validation
-
-import users.model.Person
-//todo should I get the validations? more SOLID
-
-//trait PersonValidator {
-//  def validate(person : Person) : Boolean
-//}
